eff2f59f3a6549884ff470cfc44be20b85b02221
Chiffelle <janischiffelle@hotmail.ch>
2020-04-22 23:00:07 +0200

    add test parameterizedTest

--- src/test/java/PlayerTest.java
+++ src/test/java/PlayerTest.java
@@ -1,4 +1,6 @@
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
 
 import static org.junit.jupiter.api.Assertions.*;
 
@@ -13,5 +15,14 @@ class PlayerTest {
         assertEquals(2000, p.getNetWorth());
         p.reduceCash(200);
         assertEquals(1800, p.getNetWorth());
+    }
+
+    @ParameterizedTest
+    @ValueSource(ints = {100, 200, 300})
+    void checkAddMultipleTime(int value)
+    {
+        int temp = p.getNetWorth();
+        p.addCash(value);
+        assertEquals(p.getNetWorth(), temp + value);
     }
 }
